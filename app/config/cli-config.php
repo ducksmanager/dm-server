09ccf2f8217038e51640f061336eaaf4b4948c42
Bruno Perel <brunoperel@gmail.com>
2016-11-20 22:13:07 +0100

    Fix entity generation given a namespace in the command line

--- app/config/cli-config.php
+++ app/config/cli-config.php
@@ -4,22 +4,55 @@ require_once __DIR__."/../Wtd.php";
 
 use Wtd\Wtd;
 
+/**
+ * @param string $modelNamespace
+ * @return bool|string
+ */
 function getSchemaConfigKey($modelNamespace) {
     $schemas = Wtd::getSchemas();
+    $modelNamespace = str_replace('\\', '', $modelNamespace);
 
-    return array_keys($schemas)[array_search($modelNamespace, array_column($schemas, 'models_namespace'))];
+    foreach($schemas as $schemaKey=>$schema) {
+        if (str_replace('\\', '', $schema['models_namespace']) === $modelNamespace) {
+            return $schemaKey;
+        }
+    }
+    return false;
 }
 
 /**
- * @return bool
+ * @param string $paramName
+ * @return string|null
+ */
+function getCommandLineParameter($paramName) {
+    $argvRegex = '#^--'.$paramName.'=(.+)$#';
+
+    $argvMatch = preg_grep($argvRegex, $_SERVER['argv']);
+    if (count($argvMatch) === 0) {
+        return null;
+    }
+    else {
+        return preg_replace(
+            $argvRegex,
+            '$1',
+            array_values($argvMatch)[0]
+        );
+    }
+}
+
+/**
+ * @return string|bool
  */
 function getSchemaConfigKeyFromCommandLine()
 {
-    $namespaceRegex = '#^--namespace=(.+)$#';
-    $modelNamespace = preg_replace($namespaceRegex, '$1', array_values(preg_grep($namespaceRegex, $_SERVER['argv']))[0]);
-
-    return getSchemaConfigKey($modelNamespace);
+    return getSchemaConfigKey(getCommandLineParameter('namespace'))
+        || getSchemaConfigKey(getCommandLineParameter('filter'));
 }
 
-$em = Wtd::createEntityManager(getSchemaConfigKeyFromCommandLine());
+$schemaConfigKey = getSchemaConfigKeyFromCommandLine();
+if ($schemaConfigKey === false) {
+    echo "Namespace in command line not found among schemas : ".$_SERVER['argv']."\n";
+}
+
+$em = Wtd::createEntityManager($schemaConfigKey);
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($em);
