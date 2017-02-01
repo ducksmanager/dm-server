f2fd738dbd2a373089c4a1a2be49c0e73fd185b0
Bruno Perel <brunoperel@gmail.com>
2017-02-01 23:10:00 +0000

    Fix cli-config.php

--- app/config/cli-config.php
+++ app/config/cli-config.php
@@ -25,21 +25,40 @@ function getSchemaConfigKey($modelNamespace) {
 
 /**
  * @param string $paramName
- * @return string|null
+ * @return string
  */
 function getCommandLineParameter($paramName) {
-    $argvRegex = '#^--'.$paramName.'=(.+)$#';
-
+    $argvRegex = '#^--' . $paramName . '=(.+)$#';
     $argvMatch = preg_grep($argvRegex, $_SERVER['argv']);
     if (count($argvMatch) === 0) {
         return null;
     }
-    else {
-        return preg_replace(
+    return [
+        array_values($argvMatch)[0],
+        preg_replace(
             $argvRegex,
             '$1',
             array_values($argvMatch)[0]
-        );
+        )
+    ];
+}
+
+/**
+ * @param string $paramName
+ * @return string|null
+ */
+function getCommandLineParameterValue($paramName) {
+    $commandLineParameter = getCommandLineParameter($paramName);
+    return is_null($commandLineParameter) ? null : $commandLineParameter[1];
+}
+
+/**
+ * @param string $parameterName
+ */
+function removeCommandLineParameter($parameterName)
+{
+    if (!is_null(getCommandLineParameterValue($parameterName))) {
+        array_splice($_SERVER['argv'], array_search(getCommandLineParameter($parameterName)[0], $_SERVER['argv']), 1);
     }
 }
 
@@ -48,19 +67,27 @@ function getCommandLineParameter($paramName) {
  */
 function getSchemaConfigKeyFromCommandLine()
 {
-    $schemaKeyFromNamespace = getSchemaConfigKey(getCommandLineParameter('namespace'));
+    $schemaKeyFromNamespace = getSchemaConfigKey(getCommandLineParameterValue('namespace'));
     if ($schemaKeyFromNamespace !== false) {
         return $schemaKeyFromNamespace;
     }
     else {
-        return getSchemaConfigKey(getCommandLineParameter('filter'));
+        return getSchemaConfigKey(getCommandLineParameterValue('filter'));
     }
 }
 
 $schemaConfigKey = getSchemaConfigKeyFromCommandLine();
+
 if ($schemaConfigKey === false) {
     echo "Namespace in command line not found among schemas : ".$_SERVER['argv']."\n";
 }
-
-$em = DmServer::createEntityManager($schemaConfigKey);
-return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($em);
+else {
+    if (!in_array('orm:generate-entities', $_SERVER['argv'])) {
+        removeCommandLineParameter('filter');
+    }
+    if (!in_array('orm:convert-mapping', $_SERVER['argv'])) {
+        removeCommandLineParameter('namespace');
+    }
+    $em = DmServer::createEntityManager($schemaConfigKey);
+    return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($em);
+}
