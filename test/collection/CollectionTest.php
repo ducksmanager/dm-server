20205dc0c5cd950a6ed78917011a9c1fd2dd33e3
Bruno Perel <brunoperel@gmail.com>
2017-02-13 23:10:00 +0000

    Refactor test suites

--- test/collection/CollectionTest.php
+++ test/collection/CollectionTest.php
@@ -4,71 +4,10 @@ namespace DmServer\Test;
 use Dm\Models\Numeros;
 use DmServer\Controllers\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Dm\Models\Users;
 use DmServer\DmServer;
 
 class CollectionTest extends TestCommon
 {
-    public function testCreateCollection() {
-        $response = $this->buildAuthenticatedService('/user/new', TestCommon::$dmUser, [], [
-            'username' => self::$defaultTestDmUserName,
-            'password' => 'dm_pass',
-            'password2' => 'dm_pass',
-            'email' => 'test@ducksmanager.net'
-        ])->call();
-        $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
-
-        /** @var Users[] $usersWithUsername */
-        $usersWithUsername = DmServer::getEntityManager(DmServer::CONFIG_DB_KEY_DM)->getRepository(Users::class)->findBy(
-            ['username' => self::$defaultTestDmUserName]
-        );
-
-        $this->assertEquals(1, count($usersWithUsername));
-        $this->assertEquals(Users::class, get_class($usersWithUsername[0]));
-        $this->assertEquals(sha1('dm_pass'), $usersWithUsername[0]->getPassword());
-    }
-
-    public function testCreateCollectionErrorDifferentPasswords() {
-        $response = $this->buildAuthenticatedService('/user/new', TestCommon::$dmUser, [], [
-            'username' => self::$defaultTestDmUserName,
-            'password' => 'dm_pass',
-            'password2' => 'dm_pass_different',
-            'email' => 'test@ducksmanager.net'
-        ])->call();
-        $this->assertEquals(Response::HTTP_PRECONDITION_FAILED, $response->getStatusCode());
-    }
-
-    public function testCreateCollectionErrorShortUsername() {
-        $response = $this->buildAuthenticatedService('/user/new', TestCommon::$dmUser, [], [
-            'username' => 'dm',
-            'password' => 'dm_pass',
-            'password2' => 'dm_pass',
-            'email' => 'test@ducksmanager.net'
-        ])->call();
-        $this->assertEquals(Response::HTTP_PRECONDITION_FAILED, $response->getStatusCode());
-    }
-
-    public function testCreateCollectionErrorShortPassword() {
-        $response = $this->buildAuthenticatedService('/user/new', TestCommon::$dmUser, [], [
-            'username' => self::$defaultTestDmUserName,
-            'password' => 'pass',
-            'password2' => 'pass',
-            'email' => 'test@ducksmanager.net'
-        ])->call();
-        $this->assertEquals(Response::HTTP_PRECONDITION_FAILED, $response->getStatusCode());
-    }
-
-    public function testCreateCollectionErrorExistingUsername() {
-        self::createTestCollection();
-        $response = $this->buildAuthenticatedService('/user/new', TestCommon::$dmUser, [], [
-            'username' => self::$defaultTestDmUserName,
-            'password' => 'dm_pass',
-            'password2' => 'dm_pass',
-            'email' => 'test@ducksmanager.net'
-        ])->call();
-        $this->assertEquals(Response::HTTP_CONFLICT, $response->getStatusCode());
-    }
-
     public function testAddIssue() {
         $this->assertEquals(0, count($this->getCurrentUserIssues()));
 
--- test/user/UserTest.php
+++ test/user/UserTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace DmServer\Test;
 
+use Dm\Models\Users;
+use DmServer\DmServer;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthTest extends TestCommon
@@ -36,5 +38,65 @@ class AuthTest extends TestCommon
         ])->call();
         $this->assertNotEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode());
         $this->assertNotEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
+    public function testCreateCollection() {
+        $response = $this->buildAuthenticatedService('/user/new', TestCommon::$dmUser, [], [
+            'username' => self::$defaultTestDmUserName,
+            'password' => 'dm_pass',
+            'password2' => 'dm_pass',
+            'email' => 'test@ducksmanager.net'
+        ])->call();
+        $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
+
+        /** @var Users[] $usersWithUsername */
+        $usersWithUsername = DmServer::getEntityManager(DmServer::CONFIG_DB_KEY_DM)->getRepository(Users::class)->findBy(
+            ['username' => self::$defaultTestDmUserName]
+        );
+
+        $this->assertEquals(1, count($usersWithUsername));
+        $this->assertEquals(Users::class, get_class($usersWithUsername[0]));
+        $this->assertEquals(sha1('dm_pass'), $usersWithUsername[0]->getPassword());
+    }
+
+    public function testCreateCollectionErrorDifferentPasswords() {
+        $response = $this->buildAuthenticatedService('/user/new', TestCommon::$dmUser, [], [
+            'username' => self::$defaultTestDmUserName,
+            'password' => 'dm_pass',
+            'password2' => 'dm_pass_different',
+            'email' => 'test@ducksmanager.net'
+        ])->call();
+        $this->assertEquals(Response::HTTP_PRECONDITION_FAILED, $response->getStatusCode());
+    }
+
+    public function testCreateCollectionErrorShortUsername() {
+        $response = $this->buildAuthenticatedService('/user/new', TestCommon::$dmUser, [], [
+            'username' => 'dm',
+            'password' => 'dm_pass',
+            'password2' => 'dm_pass',
+            'email' => 'test@ducksmanager.net'
+        ])->call();
+        $this->assertEquals(Response::HTTP_PRECONDITION_FAILED, $response->getStatusCode());
+    }
+
+    public function testCreateCollectionErrorShortPassword() {
+        $response = $this->buildAuthenticatedService('/user/new', TestCommon::$dmUser, [], [
+            'username' => self::$defaultTestDmUserName,
+            'password' => 'pass',
+            'password2' => 'pass',
+            'email' => 'test@ducksmanager.net'
+        ])->call();
+        $this->assertEquals(Response::HTTP_PRECONDITION_FAILED, $response->getStatusCode());
+    }
+
+    public function testCreateCollectionErrorExistingUsername() {
+        self::createTestCollection();
+        $response = $this->buildAuthenticatedService('/user/new', TestCommon::$dmUser, [], [
+            'username' => self::$defaultTestDmUserName,
+            'password' => 'dm_pass',
+            'password2' => 'dm_pass',
+            'email' => 'test@ducksmanager.net'
+        ])->call();
+        $this->assertEquals(Response::HTTP_CONFLICT, $response->getStatusCode());
     }
 }
