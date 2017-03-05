823e86f7e83102b4d8f4ebe664c0c1d4bc1314ba
Bruno Perel <brunoperel@gmail.com>
2017-03-05 23:15:00 +0000

    Generate the DB config in update-schemas.sh as well

--- scripts/update-schemas.sh
+++ scripts/update-schemas.sh
@@ -2,12 +2,19 @@
 
 app_dir=/var/www/html/dm-server/app
 
-cd ${app_dir}
-grep -Po '^models_namespace=\K.+' config/schemas.ini | \
-  while read -r namespace; do
-    ../vendor/bin/doctrine orm:schema-tool:update --namespace=${namespace}
-    if [ $? -ne 0 ]; then
-      echo "Schema creation failed."
-      exit -1
-    fi
-  done
+cd ${app_dir}/config
+php generate-db-config.php
+if [ $? -eq 0 ]; then
+  cd ${app_dir}
+  grep -Po '^models_namespace=\K.+' config/schemas.ini | \
+    while read -r namespace; do
+      ../vendor/bin/doctrine orm:schema-tool:update --namespace=${namespace}
+      if [ $? -ne 0 ]; then
+        echo "Schema creation failed."
+        exit -1
+      fi
+    done
+else
+  echo "DB config generation failed."
+  exit -1
+fi
