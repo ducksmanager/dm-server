19e417c7f769bc8e4d1042bfa4e8a564ca4ae18f
Bruno Perel <brunoperel@gmail.com>
2017-03-05 23:10:00 +0000

    Add update-schemas.sh

---
+++ scripts/update-schemas.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+app_dir=/var/www/html/dm-server/app
+
+cd ${app_dir}
+grep -Po '^models_namespace=\K.+' config/schemas.ini | \
+  while read -r namespace; do
+    ../vendor/bin/doctrine orm:schema-tool:update --namespace=${namespace}
+    if [ $? -ne 0 ]; then
+      echo "Schema creation failed."
+      exit -1
+    fi
+  done
