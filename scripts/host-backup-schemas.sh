47ec9e13c063e01b8fa8cd688d4e661cf3547d5f
Bruno Perel <brunoperel@gmail.com>
2017-05-02 23:05:00 +0000

    Allow to send DB backups to a remote server

--- scripts/host-backup-schemas.sh
+++ scripts/host-backup-schemas.sh
@@ -2,28 +2,42 @@
 
 container_name=$1
 backup_dir=$2
+remote_backup_config=$3 # For instance user@192.168.0.2:/home/user/workspace/mybackup
 config_file_path=dm-server/app/config/config.db.ini
+today=`date +%Y-%m-%d`
 
 if [ -z "$container_name" ]; then
-	echo "No container name provided. Usage : $0 <container_name> <backup_dir>"
-	exit 1
+  echo "No container name provided. Usage : $0 <container_name> <backup_dir>"
+  exit 1
 fi
 
 if [ -z "$backup_dir" ]; then
-	echo "No backup directory provided. Usage : $0 <container_name> <backup_dir>"
-	exit 1
+  echo "No backup directory provided. Usage : $0 <container_name> <backup_dir>"
+  exit 1
 fi
 
 docker exec ${container_name} grep -Po '^(host|dbname|username|password)=\K.+' ${config_file_path} | sed '$!N;$!N;$!N;s/\n/\t/g' | \
   while read -r host dbname username password; do
-    backup_file=${backup_dir}/backup_${container_name}_${dbname}.sql
-    echo "Backing up $dbname from $host to $backup_file"
-    docker exec ${container_name} mysqldump -u${username} -p${password} -h ${host} ${dbname} > ${backup_file}
-    echo "Compressing $backup_file"
-    time 7z a -m0=lzma2 ${backup_file}.7z ${backup_file}
+    backup_file=backup_${container_name}_${dbname}.sql
+    backup_path=${backup_dir}/${backup_file}
+
+    echo "Backing up $dbname from $host to $backup_path"
+    docker exec ${container_name} mysqldump -u${username} -p${password} -h ${host} ${dbname} > ${backup_path}
+    echo "Compressing $backup_path"
+    time 7z a -m0=lzma2 ${backup_path}.7z ${backup_path}
     if [ $? -eq 0 ]; then
-      rm -f ${backup_file}
-      echo "Done"
+      rm -f ${backup_path}
+      echo "Backed up locally"
+      if [ -z "$remote_backup_config" ]; then
+        echo "No remote backup configuration was provided, skipping remote backup"
+      else
+        scp ${backup_path}.7z ${remote_backup_config}/${today}-${backup_file}.7z
+        if [ $? -eq 0 ]; then
+          echo "Backed up remotely"
+        else
+          echo "Remote backup failed."
+        fi
+      fi
     else
       echo "Backup compression failed."
     fi
