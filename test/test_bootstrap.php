5af223c82a1c6e76952772bbff2b466719affbc3
Bruno Perel <brunoperel@gmail.com>
2017-07-16 23:10:00 +0000

    Don't verify peer

--- test/test_bootstrap.php
+++ test/test_bootstrap.php
@@ -1,2 +1,10 @@
 <?php
+$cacertPath = getcwd().DIRECTORY_SEPARATOR.'cacert.pem';
+if (!file_exists($cacertPath)) {
+    file_put_contents($cacertPath, file_get_contents('https://curl.haxx.se/ca/cacert.pem', false, stream_context_create([
+        'verifypeer' => false,
+        'verifyhost' => false
+    ])));
+}
+
 $loader = require __DIR__ . '/../vendor/autoload.php';
