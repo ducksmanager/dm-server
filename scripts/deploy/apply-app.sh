d622b5357f7ee8f7e4456f6f0c2ac38cb18aff55
Bruno Perel <brunoperel@gmail.com>
2017-11-10 19:40:57 +0100

    Include the recreation of Swagger in the deployment script

--- scripts/deploy/apply-app.sh
+++ scripts/deploy/apply-app.sh
@@ -10,6 +10,9 @@ touch development.log pimple.json && \
 chown www-data:www-data development.log pimple.json && \
 \
 composer update --no-dev -o && \
+echo "Generating swagger.json..." && \
+php vendor/radebatz/silex2swagger/bin/silex2swagger silex2swagger:build --file=swagger.json --path=app/controllers && \
+echo "Done." && \
 \
 bash -x scripts/update-schemas.sh 0 && \
 echo -e "\nThe schema update has to be applied now. Afterwards press y to continue the deployment process. Continue ? (y/n)" && read answer
