962add8d801396738058f8deb82fdea6eca389be
Bruno Perel <bruno@klarity.org>
2018-08-13 23:30:00 +0200

    Rely on the docker-compose config to backup and restore DBs

--- scripts/host-backup-schemas.sh
+++ scripts/host-backup-schemas.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
 
-container_name=$1
-backup_dir=$2
-remote_backup_config=$3 # For instance user@192.168.0.2:/home/user/workspace/mybackup
+backup_dir=$1
+remote_backup_config=$2 # For instance user@192.168.0.2:/home/user/workspace/mybackup
 config_file_path=app/config/config.db.ini
 today=`date +%Y-%m-%d`
 
-if [ -z "$container_name" ]; then
-  echo "No container name provided. Usage : $0 <container_name> <backup_dir>"
-  exit 1
-fi
+getEnv() {
+  while read envVariableName; do
+    docker inspect --format '{{ .Config.Env }}' ${host} |  tr ' ' '\n' | grep ${envVariableName} | sed 's/^.*=//'
+  done
+}
 
 if [ -z "$backup_dir" ]; then
   echo "No backup directory provided. Usage : $0 <container_name> <backup_dir>"
   exit 1
 fi
 
-docker exec ${container_name} grep -Po '^(host|dbname|username|password)=\K.+' ${config_file_path} | sed '$!N;$!N;$!N;s/\n/\t/g' | \
-  while read -r host dbname username password; do
+docker-compose config --services | grep '^db' | \
+  while read -r service; do
+    host=`docker-compose ps -q ${service}`
+    dbname=`echo MYSQL_DATABASE | getEnv`
+    db_password=`echo MYSQL_ROOT_PASSWORD | getEnv`
     backup_subdir=${backup_dir}/db_csv_${dbname}
+    mkdir -p ${backup_subdir}
 
     echo "Backing up $dbname from $host to $backup_subdir"
+
     rm -rf ${backup_subdir}/* && \
     docker exec ${host} /bin/bash -c "rm -rf /tmp/export && mkdir -p /tmp/export && chmod 777 /tmp/export" && \
-    docker exec ${host} /bin/bash -c "mysqldump -uroot -pchangeme --tab=/tmp/export --skip-dump-date ${dbname} && for i in /tmp/export/*.txt; do mv \$i \"$(basename \$i .txt).csv\"; done" && \
+    docker exec ${host} /bin/bash -c "mysqldump -uroot -p${db_password} --tab=/tmp/export --skip-dump-date ${dbname} && for i in /tmp/export/*.txt; do mv \$i \"$(basename \$i .txt).csv\"; done" && \
     docker cp ${host}:/tmp/export ${backup_subdir}
 
     rm -rf ${backup_subdir}/inducks_*nofulltext.*
 
     echo "Compressing backup_subdir"
-    backup_file="backup_dm-server-box_${dbname}.7z"
+    backup_file="${backup_dir}/backup_dm-server-box_${dbname}.7z"
     rm -f ${backup_file} && 7z a -t7z ${backup_file} -m0=lzma2 -mx=9 -aoa -mfb=64 -md=32m ${backup_subdir}
     if [ $? -eq 0 ]; then
       echo "Backed up locally"
@@ -47,4 +52,4 @@ docker exec ${container_name} grep -Po '^(host|dbname|username|password)=\K.+' $
     else
       echo "Backup compression failed."
     fi
-  done
+done
--- scripts/host-restore-schemas.sh
+++ scripts/host-restore-schemas.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 source_container_name=$1
-target_container_name=$2
-backup_dir=$3
+backup_dir=$2
 config_file_path=app/config/config.db.ini
+
+getEnv() {
+  while read envVariableName; do
+    docker inspect --format '{{ .Config.Env }}' ${host} |  tr ' ' '\n' | grep ${envVariableName} | sed 's/^.*=//'
+  done
+}
 
 usage="Usage : $0 <source_container_name> <target_container_name> <backup_dir>"
 
 if [ -z "$source_container_name" ]; then
-	echo "No container name provided. Usage : $usage"
-	exit 1
-fi
-
-if [ -z "$target_container_name" ]; then
 	echo "No container name provided. Usage : $usage"
 	exit 1
 fi
@@ -22,16 +22,19 @@ if [ -z "$backup_dir" ]; then
 	exit 1
 fi
 
-docker exec ${target_container_name} grep -Po '^(host|dbname|username|password)=\K.+' ${config_file_path} | sed '$!N;$!N;$!N;s/\n/\t/g' | \
-  while read -r host dbname username password; do
+docker-compose config --services | grep '^db' | \
+  while read -r service; do
+    host=`docker-compose ps -q ${service}`
+    dbname=`echo MYSQL_DATABASE | getEnv`
+    db_password=`echo MYSQL_ROOT_PASSWORD | getEnv`
     backup_filename=backup_${source_container_name}_${dbname}.sql
     backup_file=${backup_dir}/${backup_filename}
     echo "Extracting $dbname from $backup_file.7z"
     7z e -y ${backup_file}.7z -o${backup_dir}
     echo "Copying $backup_file to $host"
-    docker cp ${backup_file} ${target_container_name}:/tmp
+    docker cp ${backup_file} ${host}:/tmp
     echo "Restoring $dbname from $backup_filename to $host"
-    docker exec ${target_container_name} /bin/bash -c "mysql -u${username} -p${password} -h ${host} ${dbname} < /tmp/$backup_filename"
+    docker exec ${host} /bin/bash -c "mysql -uroot -p${db_password} -h ${host} ${dbname} < /tmp/$backup_filename"
     if [ $? -eq 0 ]; then
       rm -f ${backup_file}
       echo "Done"
