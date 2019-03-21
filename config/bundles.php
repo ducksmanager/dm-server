48ced80f758aa981b497496e2200f5b3f1041644
Bruno Perel <bruno@klarity.org>
2019-03-21 22:15:00 +0000

    Add json-request-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    SymfonyBundles\JsonRequestBundle\SymfonyBundlesJsonRequestBundle::class => ['all' => true],
 ];
