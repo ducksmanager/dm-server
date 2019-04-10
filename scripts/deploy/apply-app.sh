07377eff156b2d4d5852d6b704478fd85e17c83f
Bruno Perel <brunoperel@gmail.com>
2019-04-10 21:49:28 +0200

    Make the var directory writable after clearing the symfony cache

--- scripts/deploy/apply-app.sh
+++ scripts/deploy/apply-app.sh
@@ -5,13 +5,13 @@ webdir_new=${webdir}_new
 
 cd ${webdir_new}
 
-mkdir var && chmod a+w -R var
-
 chmod -R +x scripts && \
 APP_ENV=prod composer install --no-dev --optimize-autoloader && \
 \
 APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
 \
+mkdir -p var && chmod a+w -R var
+
 bash scripts/update-schemas.sh 0 && \
 echo -e "\nThe schema update has to be applied now. Afterwards press y to continue the deployment process. Continue ? (y/n)" && read answer && if echo "$answer" | grep -iq "^y" ;then
   rm -rf ${webdir} && mv ${webdir_new} ${webdir} && \
