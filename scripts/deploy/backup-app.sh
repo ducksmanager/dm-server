9e3a770c828a2cfbca0d181dfda722be8e7b93ef
Bruno Perel <brunoperel@gmail.com>
2019-04-11 15:11:50 +0200

    Deploy translations

--- scripts/deploy/backup-app.sh
+++ scripts/deploy/backup-app.sh
@@ -4,7 +4,7 @@ webdir=/var/www/html
 
 mkdir -p ${webdir}_old && rm -rf ${webdir}_old/*
 
-for f in bin config scripts src .env .env.local .htaccess composer.json composer.lock docker-compose.yml deployment_commit_id.txt favicon.ico
+for f in bin config scripts src translations .env .env.local .htaccess composer.json composer.lock docker-compose.yml deployment_commit_id.txt favicon.ico
 do
   if [ -d "${webdir}/$f" ] || [ -f "${webdir}/$f" ]; then
     cp -rp "${webdir}/$f" ${webdir}_old
--- scripts/deploy/restore-app.sh
+++ scripts/deploy/restore-app.sh
@@ -5,12 +5,12 @@ deployment_commit_id=$1
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-for f in bin config scripts src .env .env.local .htaccess composer.json composer.lock docker-compose.yml deployment_commit_id.txt favicon.ico
+for f in bin config scripts src translations .env .env.local .htaccess composer.json composer.lock docker-compose.yml deployment_commit_id.txt favicon.ico
 do
   rm -rf "${webdir}/$f"
 done
 
-for f in bin config scripts src .env .env.local .htaccess composer.json composer.lock docker-compose.yml deployment_commit_id.txt favicon.ico
+for f in bin config scripts src translations .env .env.local .htaccess composer.json composer.lock docker-compose.yml deployment_commit_id.txt favicon.ico
 do
   if [ -d "${webdir}_old/$f" ] || [ -f "${webdir}_old/$f" ]; then
     cp -rp "${webdir}_old/$f" "${webdir}"
