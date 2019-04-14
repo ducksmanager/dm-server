ef38469f455efc94f06b0639e2f9744c6ed954ab
Bruno Perel <brunoperel@gmail.com>
2019-04-14 18:58:38 +0200

    Only upload and generate sprites of a single edge at a time Allow generating full sprites if they contain less than 100 edges Store the actual size of sprites instead of the maximum amount of edges that they can contain

--- src/Controller/EdgeSpritesController.php
+++ src/Controller/EdgeSpritesController.php
@@ -7,43 +7,43 @@ use App\Entity\Dm\TranchesPretesSprites;
 use App\Entity\Dm\TranchesPretesSpritesUrls;
 use App\Helper\SpriteHelper;
 use Doctrine\ORM\ORMException;
-use Doctrine\ORM\Query\Expr\OrderBy;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EdgeSpritesController extends AbstractController implements RequiresDmVersionController
 {
-    static $SPRITE_SIZES = [ 10, 20, 50, 100, 'full'];
+    private static $SPRITE_SIZES = [ 10, 20, 50, 100, 'full'];
+    private static $MAX_SPRITE_SIZE = 100;
 
     /**
-     * @Route(methods={"PUT"}, path="/edgesprites/from/{fromEdgeID}")
+     * @Route(methods={"PUT"}, path="/edgesprites/{edgeID}")
      * @throws ORMException
      */
-    public function uploadEdgesAndGenerateSprites(LoggerInterface $logger, string $fromEdgeID) {
-        $uploadEdgesResult = $this->uploadEdges($logger, $fromEdgeID);
-        $updateTagsResult = $this->updateTags($logger, $fromEdgeID);
-        $generateSpritesResult = $this->generateSprites($logger, $fromEdgeID);
+    public function uploadEdgesAndGenerateSprites(LoggerInterface $logger, string $edgeID) {
+        $uploadEdgesResult = $this->uploadEdges($logger, $edgeID);
+        $updateTagsResult = $this->updateTags($logger, $edgeID);
+        $generateSpritesResult = $this->generateSprites($logger, $edgeID);
 
         return new JsonResponse([
-            'edgesToUpload' => (array) json_decode($uploadEdgesResult->getContent()),
-            'spriteNames' => (array) json_decode($updateTagsResult->getContent()),
-            'createdSprites' => (array) json_decode($generateSpritesResult->getContent()),
+            'edgesToUpload' => json_decode($uploadEdgesResult->getContent(), true),
+            'spriteNames' => json_decode($updateTagsResult->getContent(), true),
+            'createdSprites' => json_decode($generateSpritesResult->getContent(), true),
         ]);
     }
 
     /**
-     * @Route(methods={"PUT"}, path="/edgesprites/upload/from/{fromEdgeID}")
+     * @Route(methods={"PUT"}, path="/edgesprites/upload/{edgeID}")
      */
-    public function uploadEdges(LoggerInterface $logger, string $fromEdgeID) {
+    public function uploadEdges(LoggerInterface $logger, string $edgeID) {
 
         $dmEm = $this->getEm('dm');
         $qb = $dmEm->createQueryBuilder();
         $qb
             ->select('edges.publicationcode, edges.issuenumber, edges.slug')
             ->from(TranchesPretes::class, 'edges')
-            ->andWhere($qb->expr()->gte('edges.id', ':fromID'))
-            ->setParameter(':fromID', $fromEdgeID);
+            ->andWhere($qb->expr()->eq('edges.id', ':fromID'))
+            ->setParameter(':fromID', $edgeID);
         $edgesToUpload = $qb->getQuery()->getArrayResult();
 
         foreach($edgesToUpload as $edgeToUpload) {
@@ -60,34 +60,49 @@ class EdgeSpritesController extends AbstractController implements RequiresDmVers
     }
 
     /**
-     * @Route(methods={"PUT"}, path="/edgesprites/tags/from/{fromEdgeID}")
+     * @Route(methods={"PUT"}, path="/edgesprites/tags/{edgeID}")
      * @throws ORMException
      */
-    public function updateTags(LoggerInterface $logger, int $fromEdgeID)
+    public function updateTags(LoggerInterface $logger, int $edgeID)
     {
-
         $TAGGABLE_ASSETS_LIMIT = 1000;
 
         $qbDeleteExistingSpriteNames = $this->getEm('dm')->createQueryBuilder();
         $qbDeleteExistingSpriteNames
             ->delete(TranchesPretesSprites::class, 'sprite')
-            ->andWhere($qbDeleteExistingSpriteNames->expr()->gte('sprite.idTranche', $fromEdgeID));
+            ->andWhere($qbDeleteExistingSpriteNames->expr()->eq('sprite.idTranche', $edgeID));
         $qbDeleteExistingSpriteNames->getQuery()->execute();
 
-        $edge = $this->getEm('dm')->getRepository(TranchesPretes::class)->find($fromEdgeID);
+        $edge = $this->getEm('dm')->getRepository(TranchesPretes::class)->find($edgeID);
 
         $spriteNames = [];
 
         foreach(self::$SPRITE_SIZES as $spriteSize) {
+            $spriteName = self::getSpriteName($edge->getPublicationcode(), $spriteSize === 'full'
+                ? 'full'
+                : self::getSpriteRange($edge->getIssuenumber(), $spriteSize)
+            );
+
             if ($spriteSize === 'full') {
-                $spriteName = self::getSpriteName($edge->getPublicationcode(), 'full');
                 $spriteSize = $this->getEm('dm')->getRepository(TranchesPretes::class)->count([
                     'publicationcode' => $edge->getPublicationcode()
                 ]);
+                if ($spriteSize > self::$MAX_SPRITE_SIZE) {
+                    $logger->info("Not creating a full sprite for publication {$edge->getPublicationcode()} : sprite size is too big ($spriteSize)");
+                    continue;
+                }
             }
             else {
-                $spriteName = self::getSpriteName($edge->getPublicationcode(), self::getSpriteRange($edge->getIssuenumber(), $spriteSize));
+                $spriteSize = $this->getEm('dm')->getRepository(TranchesPretesSprites::class)->count([
+                    'spriteName' => $spriteName
+                ]) + 1;
             }
+
+            $logger->info("Adding tag $spriteName on {$edge->getSlug()}");
+            SpriteHelper::add_tag(
+                $spriteName,
+                $edge->getSlug()
+            );
 
             $spriteForEdge = new TranchesPretesSprites();
             $this->getEm('dm')->persist($spriteForEdge
@@ -95,52 +110,26 @@ class EdgeSpritesController extends AbstractController implements RequiresDmVers
                 ->setSpriteName($spriteName)
                 ->setSpriteSize($spriteSize)
             );
-            $logger->info("Adding tag $spriteName on {$edge->getSlug()}");
-            SpriteHelper::add_tag(
-                $spriteName,
-                $edge->getSlug()
-            );
+
+            $qbUpdateOtherEdgesInSprite = $this->getEm('dm')->createQueryBuilder()
+                ->update(TranchesPretesSprites::class, 'otherEdgeInSprite')
+                ->set('otherEdgeInSprite.spriteSize', $spriteSize)
+                ->andWhere($qbDeleteExistingSpriteNames->expr()->eq('otherEdgeInSprite.spriteName', $qbDeleteExistingSpriteNames->expr()->literal($spriteName)));
+            $qbUpdateOtherEdgesInSprite->getQuery()->execute();
+
             $this->getEm('dm')->flush();
 
-            $spriteNames[] = $spriteName;
-        }
-
-
-        $qb = $this->getEm('dm')->createQueryBuilder();
-        $qb
-            ->select('sprite')
-            ->from(TranchesPretesSprites::class, 'sprite')
-            ->andWhere($qb->expr()->gte('sprite.idTranche', $fromEdgeID))
-            ->orderBy(new OrderBy('sprite.spriteName', 'ASC'));
-
-        /** @var TranchesPretesSprites[] $allSpritesAndEdges */
-        $allSpritesAndEdges = $qb->getQuery()->getResult();
-
-        $slugsPerSprite = [];
-        foreach ($allSpritesAndEdges as $spriteAndEdge) {
-            $slugsPerSprite[$spriteAndEdge->getSpriteName()][] = $spriteAndEdge->getIdTranche()->getSlug();
-        }
-
-        /** @var string[] $edgeSlugs */
-        foreach ($slugsPerSprite as $spriteName => $edgeSlugs) {
-            $numberOfEdges = count($edgeSlugs);
-            $logger->info("Adding tag $spriteName on $numberOfEdges edges");
-            for ($i = 0; $i < $numberOfEdges; $i += $TAGGABLE_ASSETS_LIMIT) {
-                SpriteHelper::add_tag(
-                    $spriteName,
-                    array_slice($edgeSlugs, $i, $TAGGABLE_ASSETS_LIMIT - 1)
-                );
-            }
+            $spriteNames[$spriteName] = (object) ['size' => $spriteSize];
         }
 
         return new JsonResponse($spriteNames);
     }
 
     /**
-     * @Route(methods={"PUT"}, path="/edgesprites/sprites/from/{fromEdgeID}")
+     * @Route(methods={"PUT"}, path="/edgesprites/sprites/{edgeID}")
      * @throws ORMException
      */
-    public function generateSprites(LoggerInterface $logger, int $fromEdgeID) {
+    public function generateSprites(LoggerInterface $logger, int $edgeID) {
 
         $dmEm = $this->getEm('dm');
         $qbExistingUrls = $dmEm->createQueryBuilder();
@@ -153,9 +142,7 @@ class EdgeSpritesController extends AbstractController implements RequiresDmVers
             ->select('distinct sprites.spriteName')
             ->from(TranchesPretesSprites::class, 'sprites')
             ->andWhere($qb->expr()->notIn('sprites.spriteName', $qbExistingUrls->getDQL()))
-            ->andWhere($qb->expr()->notLike('sprites.spriteName', ':fullSprite'))
-            ->andWhere($qb->expr()->gte('sprites.idTranche', $fromEdgeID))
-            ->setParameter(':fullSprite', '%-full');
+            ->andWhere($qb->expr()->eq('sprites.idTranche', $edgeID));
 
         $spritesWithNoUrl = $qb->getQuery()->getResult();
         foreach($spritesWithNoUrl as $sprite) {
@@ -173,10 +160,12 @@ class EdgeSpritesController extends AbstractController implements RequiresDmVers
             );
             $dmEm->flush();
         }
-        return new JsonResponse($spritesWithNoUrl);
+        return new JsonResponse(array_map(function($spriteWithNoUrl) {
+            return $spriteWithNoUrl['spriteName'];
+        }, $spritesWithNoUrl));
     }
 
-    static function getSpriteRange(string $issueNumber, int $rangeWidth): string
+    private static function getSpriteRange(string $issueNumber, int $rangeWidth): string
     {
         $issueNumberInt = (int)$issueNumber;
         return implode('-', [
@@ -185,7 +174,7 @@ class EdgeSpritesController extends AbstractController implements RequiresDmVers
         ]);
     }
 
-    static function getSpriteName(string $publicationCode, string $suffix): string
+    public static function getSpriteName(string $publicationCode, string $suffix): string
     {
         return implode('-', [
             'edges',
--- tests/Controller/EdgeSpritesTest.php
+++ tests/Controller/EdgeSpritesTest.php
@@ -2,8 +2,10 @@
 namespace App\Tests;
 
 use App\Entity\Dm\TranchesPretes;
+use App\Entity\Dm\TranchesPretesSprites;
 use App\Helper\SpriteHelper;
 use App\Tests\Fixtures\EdgesFixture;
+use DateTime;
 
 class EdgeSpritesTest extends TestCommon
 {
@@ -22,15 +24,15 @@ class EdgeSpritesTest extends TestCommon
             'upload' => 'Success',
             'add_tag' => 'Success',
             'generate_sprite' => [
-                'edges-fr-JM-3001-3010' => ['version' => 123456789],
-                'edges-fr-JM-3001-3020' => ['version' => 123456789],
-                'edges-fr-JM-3001-3050' => ['version' => 123456789],
-                'edges-fr-JM-3001-3100' => ['version' => 123456789],
-                'edges-fr-JM-full' => ['version' => 123456789]
+                'edges-fr-JM-3001-3010' => ['version' => 1],
+                'edges-fr-JM-3001-3020' => ['version' => 2],
+                'edges-fr-JM-3001-3050' => ['version' => 3],
+                'edges-fr-JM-3001-3100' => ['version' => 4],
+                'edges-fr-JM-full' => ['version' => 5]
             ]
         ];
 
-        $response = $this->buildAuthenticatedServiceWithTestUser("/edgesprites/from/{$edge->getId()}", self::$adminUser, 'PUT')->call();
+        $response = $this->buildAuthenticatedServiceWithTestUser("/edgesprites/{$edge->getId()}", self::$adminUser, 'PUT')->call();
         $objectResponse = json_decode($this->getResponseContent($response), false);
         $this->assertEquals((object) [
             'edgesToUpload' => [
@@ -39,24 +41,82 @@ class EdgeSpritesTest extends TestCommon
                     'issuenumber' => '3001',
                     'slug' => 'edges-fr-JM-3001',
                 ],
-                (object) [
-                    'publicationcode' => 'fr/JM',
-                    'issuenumber' => '4001',
-                    'slug' => 'edges-fr-JM-4001',
-                ],
             ],
-            'spriteNames' => [
+            'spriteNames' => (object)[
+                'edges-fr-JM-3001-3010' => (object) ['size' => 1],
+                'edges-fr-JM-3001-3020' => (object) ['size' => 1],
+                'edges-fr-JM-3001-3050' => (object) ['size' => 1],
+                'edges-fr-JM-3001-3100' => (object) ['size' => 1],
+                'edges-fr-JM-full' => (object) ['size' => 2],
+            ],
+            'createdSprites' => [
                 'edges-fr-JM-3001-3010',
                 'edges-fr-JM-3001-3020',
                 'edges-fr-JM-3001-3050',
                 'edges-fr-JM-3001-3100',
                 'edges-fr-JM-full',
             ],
+        ], $objectResponse);
+    }
+
+    public function testUploadEdgeAndGenerateSpriteNoFullSprite() {
+        $this->loadFixture('dm', new EdgesFixture());
+
+        for ($i=2000; $i<2100; $i++) {
+            $edge = new TranchesPretes();
+            $this->getEm('dm')->persist($edge
+                ->setPublicationcode('fr/JM')
+                ->setIssuenumber($i)
+                ->setDateajout(new DateTime())
+            );
+
+            $edgeSprite = new TranchesPretesSprites();
+            $this->getEm('dm')->persist($edgeSprite
+                ->setIdTranche($edge)
+                ->setSpriteSize(150)
+                ->setSpriteName('edges-fr-JM-full')
+            );
+        }
+
+        $this->getEm('dm')->flush();
+
+        $edge = $this->getEm('dm')->getRepository(TranchesPretes::class)->findOneBy([
+            'publicationcode' => 'fr/JM',
+            'issuenumber' => '3001'
+        ]);
+
+        SpriteHelper::$mockedResults = [
+            'upload' => 'Success',
+            'add_tag' => 'Success',
+            'generate_sprite' => [
+                'edges-fr-JM-3001-3010' => ['version' => 1],
+                'edges-fr-JM-3001-3020' => ['version' => 2],
+                'edges-fr-JM-3001-3050' => ['version' => 3],
+                'edges-fr-JM-3001-3100' => ['version' => 4]
+            ]
+        ];
+
+        $response = $this->buildAuthenticatedServiceWithTestUser("/edgesprites/{$edge->getId()}", self::$adminUser, 'PUT')->call();
+        $objectResponse = json_decode($this->getResponseContent($response), false);
+        $this->assertEquals((object) [
+            'edgesToUpload' => [
+                (object) [
+                    'publicationcode' => 'fr/JM',
+                    'issuenumber' => '3001',
+                    'slug' => 'edges-fr-JM-3001',
+                ],
+            ],
+            'spriteNames' => (object)[
+                'edges-fr-JM-3001-3010' => (object) ['size' => 1],
+                'edges-fr-JM-3001-3020' => (object) ['size' => 1],
+                'edges-fr-JM-3001-3050' => (object) ['size' => 1],
+                'edges-fr-JM-3001-3100' => (object) ['size' => 1]
+            ],
             'createdSprites' => [
-                (object) ['spriteName' => 'edges-fr-JM-3001-3010'],
-                (object) ['spriteName' => 'edges-fr-JM-3001-3020'],
-                (object) ['spriteName' => 'edges-fr-JM-3001-3050'],
-                (object) ['spriteName' => 'edges-fr-JM-3001-3100'],
+                'edges-fr-JM-3001-3010',
+                'edges-fr-JM-3001-3020',
+                'edges-fr-JM-3001-3050',
+                'edges-fr-JM-3001-3100',
             ],
         ], $objectResponse);
     }
