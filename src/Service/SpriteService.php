d7b80a0d6356c6b214b07582126679c5d24b943e
Bruno Perel <brunoperel@gmail.com>
2020-11-11 15:38:28 +0100

    Fix Cloudinary URL not retrieved by Cloudinary API

--- src/Service/SpriteService.php
+++ src/Service/SpriteService.php
@@ -8,14 +8,17 @@ class SpriteService {
     public static ?array $mockedResults = null;
 
     public function upload($file, $options = array()) {
+        putenv('CLOUDINARY_URL='.$_ENV['CLOUDINARY_URL']);
         return self::$mockedResults['upload'] ?: Uploader::upload($file, $options);
     }
 
     public function add_tag($tag, $public_ids = array(), $options = array()) {
+        putenv('CLOUDINARY_URL='.$_ENV['CLOUDINARY_URL']);
         return self::$mockedResults['add_tag'] ?: Uploader::add_tag($tag, $public_ids, $options);
     }
 
     public function generate_sprite($tag, $options = array()) {
+        putenv('CLOUDINARY_URL='.$_ENV['CLOUDINARY_URL']);
         return self::$mockedResults['generate_sprite'][$tag] ?: Uploader::generate_sprite($tag, $options);
     }
 }
