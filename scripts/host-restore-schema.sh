98bb73ab0ff131a676570df0f783912d5f3f77a3
Bruno Perel <brunoperel@gmail.com>
2022-07-21 11:40:23 +0200

    Adapt DB scripts to use a single DB server

--- scripts/host-restore-schema.sh
+++ scripts/host-restore-schema.sh
@@ -16,7 +16,7 @@ fi
 db_password=$(getEnv MYSQL_ROOT_PASSWORD)
 backup_file="$backup_dir/backup_dm-server-db.sql.gz"
 
-zcat "${backup_file}" | docker exec -i db mysql -u root -p"${db_password}" db
+zcat "${backup_file}" | docker exec -i db mysql -u root -p"${db_password}"
 if [ $? -eq 0 ]; then
   echo "Done"
 else
