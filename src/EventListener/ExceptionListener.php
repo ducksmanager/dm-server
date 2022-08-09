49f16a151ec3a1ccb2f6f0d148e13cbed250e6f0
Bruno Perel <brunoperel@gmail.com>
2022-08-09 10:19:28 +0200

    Revert "Bump dependencies"
    This reverts commit 536261d93177e8251c420d0bc1d71021aae929fa.

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpKernel\Event\RequestEvent;
 class ExceptionListener {
     public function __invoke(RequestEvent $event): void
     {
-        if (!$event->isMainRequest()) {
+        if (!$event->isMasterRequest()) {
             $exception = $event->getRequest()->attributes->get('exception');
             if (!is_null($exception)) {
                 $event->setResponse(new Response($exception->getMessage()));
